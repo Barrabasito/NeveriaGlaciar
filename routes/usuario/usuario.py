1b85c798c7be788c691fc27f39d6b31779601cef
Fermin921 <ferminmartinez921@gmail.com>
2023-12-05 09:46:54 -0600

    OTRO

--- routes/usuario/usuario.py
+++ routes/usuario/usuario.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, render_template, redirect
 from sqlalchemy import exc
-from models import Usuario,Sucursal
+from models import Usuario, Sucursal
 from app import db, bcrypt
 from auth import tokenCheck, verificar
 from config import BaseConfig
@@ -51,7 +51,12 @@ def indexUsuario():
     else:
         nombre_usuario = request.json["email"]
         password = request.json["password"]
-        usuario = Usuario(nombre_usuario=nombre_usuario, password=password,admin=True,sucursal_id=None)
+        usuario = Usuario(
+            nombre_usuario=nombre_usuario,
+            password=password,
+            admin=True,
+            sucursal_id=None,
+        )
         searchUser = Usuario.query.filter_by(nombre_usuario=nombre_usuario).first()
         if searchUser:
             validation = bcrypt.check_password_hash(searchUser.password, password)
@@ -65,22 +70,27 @@ def indexUsuario():
                 return jsonify(response)
         return jsonify({"message": "Datos incorrectos"})
 
+
 @appusuario.route("/agregarUsuario", methods=["GET", "POST"])
 def agregar_usuario():
     if request.method == "GET":
         sucursales = Sucursal.query.all()
-        return render_template("agregarUsuario.html",sucursales=sucursales)
+        return render_template("agregarUsuario.html", sucursales=sucursales)
     else:
         nombre_usuario = request.json["nombre_usuario"]
         password = request.json["password"]
-        admin = request.json.get("admin", False)  # Puedes establecer un valor predeterminado si no se proporciona
-        sucursal_id = request.json.get("sucursal_id", None)  # Puedes establecer None si no se proporciona
+        admin = request.json.get(
+            "admin", False
+        )  # Puedes establecer un valor predeterminado si no se proporciona
+        sucursal_id = request.json.get(
+            "sucursal_id", None
+        )  # Puedes establecer None si no se proporciona
 
         usuario = Usuario(
             nombre_usuario=nombre_usuario,
             password=password,
             admin=admin,
-            sucursal_id=sucursal_id
+            sucursal_id=sucursal_id,
         )
 
         user_exists = Usuario.query.filter_by(nombre_usuario=nombre_usuario).first()
@@ -105,15 +115,16 @@ def consulta_usuarios():
     usuarios = Usuario.query.all()
     usuarios_data = [
         {
-            'id_usuario': usuario.id_usuario,
-            'nombre_usuario': usuario.nombre_usuario,
-            'password': usuario.password,
-            'fecha_registro': usuario.fecha_registro.isoformat(),
-            'admin': usuario.admin,
-            'sucursal_id': usuario.sucursal_id
-        } for usuario in usuarios
+            "id_usuario": usuario.id_usuario,
+            "nombre_usuario": usuario.nombre_usuario,
+            "password": usuario.password,
+            "fecha_registro": usuario.fecha_registro.isoformat(),
+            "admin": usuario.admin,
+            "sucursal_id": usuario.sucursal_id,
+        }
+        for usuario in usuarios
     ]
-    return jsonify({'usuarios': usuarios_data})
+    return jsonify({"usuarios": usuarios_data})
 
 
 @appusuario.route("/detalleUsuario/<int:id_usuario>")
@@ -121,12 +132,14 @@ def ver_usuario(id_usuario):
     usuario = Usuario.query.get_or_404(id_usuario)
 
     usuario_data = {
-        'id_usuario': usuario.id_usuario,
-        'nombre_usuario': usuario.nombre_usuario,
-        'password': usuario.password,
-        'fecha_registro': usuario.fecha_registro.strftime('%Y-%m-%d'),  # Formatear la fecha como string
-        'admin': usuario.admin,
-        'sucursal_id': usuario.sucursal_id
+        "id_usuario": usuario.id_usuario,
+        "nombre_usuario": usuario.nombre_usuario,
+        "password": usuario.password,
+        "fecha_registro": usuario.fecha_registro.strftime(
+            "%Y-%m-%d"
+        ),  # Formatear la fecha como string
+        "admin": usuario.admin,
+        "sucursal_id": usuario.sucursal_id,
     }
 
     return render_template("detalleUsuario.html", usuario=usuario_data)
@@ -146,7 +159,12 @@ def editar_usuario(id_usuario):
             else:
                 isAdmin = 0
             print(isAdmin)
-            return render_template("editarUsuario.html", usuario=usuario, isAdmin=isAdmin, sucursales=sucursales)
+            return render_template(
+                "editarUsuario.html",
+                usuario=usuario,
+                isAdmin=isAdmin,
+                sucursales=sucursales,
+            )
 
         else:
             nombre_usuario = request.json.get("nombre_usuario")
@@ -155,23 +173,38 @@ def editar_usuario(id_usuario):
             sucursal_id = request.json.get("sucursal_id")
 
             # Verificar si el nuevo nombre de usuario ya existe
-            existing_user = Usuario.query.filter(Usuario.nombre_usuario == nombre_usuario, Usuario.id_usuario != id_usuario).first()
+            existing_user = Usuario.query.filter(
+                Usuario.nombre_usuario == nombre_usuario,
+                Usuario.id_usuario != id_usuario,
+            ).first()
             if existing_user:
-                responseObject = {"status": "error", "message": "El nombre de usuario ya está en uso"}
+                responseObject = {
+                    "status": "error",
+                    "message": "El nombre de usuario ya está en uso",
+                }
                 return jsonify(responseObject)
 
             usuario.nombre_usuario = nombre_usuario
-            usuario.password = bcrypt.generate_password_hash(password, BaseConfig.BCRYPT_LOG_ROUNDS).decode()
+            usuario.password = bcrypt.generate_password_hash(
+                password, BaseConfig.BCRYPT_LOG_ROUNDS
+            ).decode()
             usuario.admin = admin
             usuario.sucursal_id = sucursal_id
 
             db.session.commit()
-            responseObject = {"status": "success", "message": "Usuario editado correctamente", "id_usuario": usuario.id_usuario}
+            responseObject = {
+                "status": "success",
+                "message": "Usuario editado correctamente",
+                "id_usuario": usuario.id_usuario,
+            }
             return jsonify(responseObject)
 
     except IntegrityError as e:
         db.session.rollback()
-        responseObject = {"status": "error", "message": "Error de integridad en la base de datos"}
+        responseObject = {
+            "status": "error",
+            "message": "Error de integridad en la base de datos",
+        }
         return jsonify(responseObject)
 
     except Exception as e:
@@ -181,3 +214,25 @@ def editar_usuario(id_usuario):
 
     finally:
         db.session.close()
+
+
+@appusuario.route("/eliminarUsuario/<int:id_usuario>", methods=["DELETE"])
+def eliminar_categoria(id_usuario):
+    try:
+        Usuarios = Usuario.query.get_or_404(id_usuario)
+
+        # Eliminar la categoría
+        db.session.delete(Usuarios)
+        db.session.commit()
+
+        responseObject = {
+            "status": "success",
+            "message": "Categoría eliminada correctamente",
+        }
+    except Exception as e:
+        db.session.rollback()
+        responseObject = {"status": "error", "message": str(e)}
+    finally:
+        db.session.close()
+
+    return jsonify(responseObject)
